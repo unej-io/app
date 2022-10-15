f6152a7bdc269774041b7d63db9de7bab4cf807a
flamrdevs <flamrdevs@gmail.com>
2022-10-15 12:55:09 +0700

    Resolve @unej-io/ui

--- src/pages/__DEV__/components/page.__dev__.components.tsx
+++ src/pages/__DEV__/components/page.__dev__.components.tsx
@@ -1,9 +1,12 @@
 import {} from "react";
 
 import {
+  Accordion,
   ActionIcon,
+  Anchor,
   Avatar,
   Button,
+  Checkbox,
   Container,
   Divider,
   Grid,
@@ -22,12 +25,13 @@ import {
   Title,
   Tooltip,
 } from "@mantine/core";
-import type { ActionIconVariant, AvatarProps, ButtonVariant } from "@mantine/core";
+import type { AnchorProps, ActionIconVariant, AvatarProps, ButtonVariant } from "@mantine/core";
 
 import { IconSettings } from "@tabler/icons";
 
 import { ColorSchemeTogglerSwitch, DefaultRadiusSelectGroup, PrimaryColorSelectGroup } from "~/components/core";
 
+const anchorVariants: AnchorProps["variant"][] = ["gradient", "link", "text"];
 const actionIconVariants: ActionIconVariant[] = ["default", "filled", "gradient", "light", "outline", "subtle", "transparent"];
 const avatarVariants: AvatarProps["variant"][] = ["filled", "gradient", "light", "outline"];
 const buttonVariants: ButtonVariant[] = ["default", "filled", "gradient", "light", "outline", "subtle"];
@@ -72,6 +76,20 @@ function DEVComponentsPage() {
           <Stack spacing="xl">
             <Paper withBorder p="xl">
               <Stack>
+                <Text size="xl">Anchor</Text>
+
+                <Group>
+                  {anchorVariants.map((variant) => (
+                    <Tooltip key={variant} label={variant} withinPortal>
+                      <Anchor variant={variant}>Link</Anchor>
+                    </Tooltip>
+                  ))}
+                </Group>
+              </Stack>
+            </Paper>
+
+            <Paper withBorder p="xl">
+              <Stack>
                 <Text size="xl">Action Icon</Text>
 
                 <Group>
@@ -116,6 +134,27 @@ function DEVComponentsPage() {
 
             <Paper withBorder p="xl">
               <Stack>
+                <Text size="xl">Accordion</Text>
+
+                <Accordion defaultValue="aaa">
+                  <Accordion.Item value="aaa">
+                    <Accordion.Control>Control AAA</Accordion.Control>
+                    <Accordion.Panel>Panel AAA</Accordion.Panel>
+                  </Accordion.Item>
+                  <Accordion.Item value="bbb">
+                    <Accordion.Control>Control BBB</Accordion.Control>
+                    <Accordion.Panel>Panel BBB</Accordion.Panel>
+                  </Accordion.Item>
+                  <Accordion.Item value="ccc">
+                    <Accordion.Control>Control CCC</Accordion.Control>
+                    <Accordion.Panel>Panel CCC</Accordion.Panel>
+                  </Accordion.Item>
+                </Accordion>
+              </Stack>
+            </Paper>
+
+            <Paper withBorder p="xl">
+              <Stack>
                 <Text size="xl">Input</Text>
 
                 <Stack>
@@ -130,6 +169,13 @@ function DEVComponentsPage() {
                   <Input.Wrapper label="Segmented Control" description="This is anonymous">
                     <SegmentedControl mt={4} data={["One", "Two", "Three"]} />
                   </Input.Wrapper>
+
+                  <Checkbox.Group label="Checkbox" description="This is anonymous" defaultValue={["a"]}>
+                    <Checkbox value="a" label="A" />
+                    <Checkbox value="b" label="B" />
+                    <Checkbox value="c" label="C" />
+                    <Checkbox value="d" label="D" />
+                  </Checkbox.Group>
 
                   <Switch.Group label="Switch" description="This is anonymous" defaultValue={["a"]}>
                     <Switch value="a" label="A" />
@@ -148,406 +194,6 @@ function DEVComponentsPage() {
               </Stack>
             </Paper>
           </Stack>
-          {/* 
-          <Stack spacing="xl">
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Action Icon</Text>
-
-                <Group>
-                  {actionIconVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <ActionIcon variant={variant}>
-                        <IconSettings />
-                      </ActionIcon>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Avatar</Text>
-
-                <Group>
-                  {avatarVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Avatar variant={variant} />
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Button</Text>
-
-                <Group>
-                  {buttonVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Button variant={variant}>{variant}</Button>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Input</Text>
-
-                <Stack>
-                  <TextInput label="Text" description="This is anonymous" placeholder="Text..." />
-
-                  <NumberInput label="Number" description="This is anonymous" placeholder="Number..." />
-
-                  <PasswordInput label="Password" description="This is anonymous" placeholder="Password..." />
-
-                  <Select label="Select" description="This is anonymous" placeholder="Pick one" data={["One", "Two", "Three"]} />
-
-                  <Input.Wrapper label="Segmented Control" description="This is anonymous">
-                    <SegmentedControl mt={4} data={["One", "Two", "Three"]} />
-                  </Input.Wrapper>
-
-                  <Switch.Group label="Switch" description="This is anonymous" defaultValue={["a"]}>
-                    <Switch value="a" label="A" />
-                    <Switch value="b" label="B" />
-                    <Switch value="c" label="C" />
-                    <Switch value="d" label="D" />
-                  </Switch.Group>
-
-                  <Radio.Group label="Radio" description="This is anonymous" defaultValue="a">
-                    <Radio value="a" label="A" />
-                    <Radio value="b" label="B" />
-                    <Radio value="c" label="C" />
-                    <Radio value="d" label="D" />
-                  </Radio.Group>
-                </Stack>
-              </Stack>
-            </Paper>
-          </Stack>
-
-          <Stack spacing="xl">
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Action Icon</Text>
-
-                <Group>
-                  {actionIconVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <ActionIcon variant={variant}>
-                        <IconSettings />
-                      </ActionIcon>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Avatar</Text>
-
-                <Group>
-                  {avatarVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Avatar variant={variant} />
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Button</Text>
-
-                <Group>
-                  {buttonVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Button variant={variant}>{variant}</Button>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Input</Text>
-
-                <Stack>
-                  <TextInput label="Text" description="This is anonymous" placeholder="Text..." />
-
-                  <NumberInput label="Number" description="This is anonymous" placeholder="Number..." />
-
-                  <PasswordInput label="Password" description="This is anonymous" placeholder="Password..." />
-
-                  <Select label="Select" description="This is anonymous" placeholder="Pick one" data={["One", "Two", "Three"]} />
-
-                  <Input.Wrapper label="Segmented Control" description="This is anonymous">
-                    <SegmentedControl mt={4} data={["One", "Two", "Three"]} />
-                  </Input.Wrapper>
-
-                  <Switch.Group label="Switch" description="This is anonymous" defaultValue={["a"]}>
-                    <Switch value="a" label="A" />
-                    <Switch value="b" label="B" />
-                    <Switch value="c" label="C" />
-                    <Switch value="d" label="D" />
-                  </Switch.Group>
-
-                  <Radio.Group label="Radio" description="This is anonymous" defaultValue="a">
-                    <Radio value="a" label="A" />
-                    <Radio value="b" label="B" />
-                    <Radio value="c" label="C" />
-                    <Radio value="d" label="D" />
-                  </Radio.Group>
-                </Stack>
-              </Stack>
-            </Paper>
-          </Stack>
-
-          <Stack spacing="xl">
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Action Icon</Text>
-
-                <Group>
-                  {actionIconVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <ActionIcon variant={variant}>
-                        <IconSettings />
-                      </ActionIcon>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Avatar</Text>
-
-                <Group>
-                  {avatarVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Avatar variant={variant} />
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Button</Text>
-
-                <Group>
-                  {buttonVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Button variant={variant}>{variant}</Button>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Input</Text>
-
-                <Stack>
-                  <TextInput label="Text" description="This is anonymous" placeholder="Text..." />
-
-                  <NumberInput label="Number" description="This is anonymous" placeholder="Number..." />
-
-                  <PasswordInput label="Password" description="This is anonymous" placeholder="Password..." />
-
-                  <Select label="Select" description="This is anonymous" placeholder="Pick one" data={["One", "Two", "Three"]} />
-
-                  <Input.Wrapper label="Segmented Control" description="This is anonymous">
-                    <SegmentedControl mt={4} data={["One", "Two", "Three"]} />
-                  </Input.Wrapper>
-
-                  <Switch.Group label="Switch" description="This is anonymous" defaultValue={["a"]}>
-                    <Switch value="a" label="A" />
-                    <Switch value="b" label="B" />
-                    <Switch value="c" label="C" />
-                    <Switch value="d" label="D" />
-                  </Switch.Group>
-
-                  <Radio.Group label="Radio" description="This is anonymous" defaultValue="a">
-                    <Radio value="a" label="A" />
-                    <Radio value="b" label="B" />
-                    <Radio value="c" label="C" />
-                    <Radio value="d" label="D" />
-                  </Radio.Group>
-                </Stack>
-              </Stack>
-            </Paper>
-          </Stack>
-
-          <Stack spacing="xl">
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Action Icon</Text>
-
-                <Group>
-                  {actionIconVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <ActionIcon variant={variant}>
-                        <IconSettings />
-                      </ActionIcon>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Avatar</Text>
-
-                <Group>
-                  {avatarVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Avatar variant={variant} />
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Button</Text>
-
-                <Group>
-                  {buttonVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Button variant={variant}>{variant}</Button>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Input</Text>
-
-                <Stack>
-                  <TextInput label="Text" description="This is anonymous" placeholder="Text..." />
-
-                  <NumberInput label="Number" description="This is anonymous" placeholder="Number..." />
-
-                  <PasswordInput label="Password" description="This is anonymous" placeholder="Password..." />
-
-                  <Select label="Select" description="This is anonymous" placeholder="Pick one" data={["One", "Two", "Three"]} />
-
-                  <Input.Wrapper label="Segmented Control" description="This is anonymous">
-                    <SegmentedControl mt={4} data={["One", "Two", "Three"]} />
-                  </Input.Wrapper>
-
-                  <Switch.Group label="Switch" description="This is anonymous" defaultValue={["a"]}>
-                    <Switch value="a" label="A" />
-                    <Switch value="b" label="B" />
-                    <Switch value="c" label="C" />
-                    <Switch value="d" label="D" />
-                  </Switch.Group>
-
-                  <Radio.Group label="Radio" description="This is anonymous" defaultValue="a">
-                    <Radio value="a" label="A" />
-                    <Radio value="b" label="B" />
-                    <Radio value="c" label="C" />
-                    <Radio value="d" label="D" />
-                  </Radio.Group>
-                </Stack>
-              </Stack>
-            </Paper>
-          </Stack>
-
-          <Stack spacing="xl">
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Action Icon</Text>
-
-                <Group>
-                  {actionIconVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <ActionIcon variant={variant}>
-                        <IconSettings />
-                      </ActionIcon>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Avatar</Text>
-
-                <Group>
-                  {avatarVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Avatar variant={variant} />
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Button</Text>
-
-                <Group>
-                  {buttonVariants.map((variant) => (
-                    <Tooltip key={variant} label={variant} withinPortal>
-                      <Button variant={variant}>{variant}</Button>
-                    </Tooltip>
-                  ))}
-                </Group>
-              </Stack>
-            </Paper>
-
-            <Paper withBorder p="xl">
-              <Stack>
-                <Text size="xl">Input</Text>
-
-                <Stack>
-                  <TextInput label="Text" description="This is anonymous" placeholder="Text..." />
-
-                  <NumberInput label="Number" description="This is anonymous" placeholder="Number..." />
-
-                  <PasswordInput label="Password" description="This is anonymous" placeholder="Password..." />
-
-                  <Select label="Select" description="This is anonymous" placeholder="Pick one" data={["One", "Two", "Three"]} />
-
-                  <Input.Wrapper label="Segmented Control" description="This is anonymous">
-                    <SegmentedControl mt={4} data={["One", "Two", "Three"]} />
-                  </Input.Wrapper>
-
-                  <Switch.Group label="Switch" description="This is anonymous" defaultValue={["a"]}>
-                    <Switch value="a" label="A" />
-                    <Switch value="b" label="B" />
-                    <Switch value="c" label="C" />
-                    <Switch value="d" label="D" />
-                  </Switch.Group>
-
-                  <Radio.Group label="Radio" description="This is anonymous" defaultValue="a">
-                    <Radio value="a" label="A" />
-                    <Radio value="b" label="B" />
-                    <Radio value="c" label="C" />
-                    <Radio value="d" label="D" />
-                  </Radio.Group>
-                </Stack>
-              </Stack>
-            </Paper>
-          </Stack> */}
         </Grid.Col>
       </Grid>
     </Container>
